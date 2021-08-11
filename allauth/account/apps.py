c66469b634a3e1070f2530ea7abdec3d384ed102
Mateo Kurti <mateo_kurti@protonmail.ch>
2021-08-11 15:32:04 +0200

    Changed the Account App Label to solve the labeling conflict
    The allauth.account app seemed to give conflict if you had a custom app named account. Adding the label and also migrating seemed to fix the issue.

--- allauth/account/apps.py
+++ allauth/account/apps.py
@@ -4,4 +4,5 @@ from django.utils.translation import gettext_lazy as _
 
 class AccountConfig(AppConfig):
     name = "allauth.account"
+    label = "allauth_account"
     verbose_name = _("Accounts")
